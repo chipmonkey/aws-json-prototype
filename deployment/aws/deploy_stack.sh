a56157f021a3d45d5e851e85f4b2a6453c53aa78
Chip Lynch <chip@chiplynch.com>
2021-01-05 20:34:27 -0800

    Glue loads to a database; athena queries work
    Made use of subfolders on S3

--- deployment/aws/deploy_stack.sh
+++ deployment/aws/deploy_stack.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #
 # Requires lambda_deployfile.zip is already pushed to the S3 deployment bucket
+# Will fail if stack already exists
 #
 set -e
 aws cloudformation validate-template --template-body file://cloudformation.yml
-aws cloudformation delete-stack --stack-name monkeyStack
-aws cloudformation wait stack-delete-complete --stack-name monkeyStack
 
 set +e
 if aws cloudformation deploy --template-file ./cloudformation.yml --stack-name monkeyStack --capabilities CAPABILITY_IAM \
