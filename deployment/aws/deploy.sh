dcbcb88c8cd44c6fdf19b840aa08b7c679cf4a81
Chip Lynch <chip@chiplynch.com>
2021-01-05 18:11:40 -0800

    Added cloudformation update capability
    For speed... use 'make deploy-update'

--- deployment/aws/deploy.sh
+++ deployment/aws/deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Requires lambda_deployfile.zip is already pushed to the S3 deployment bucket
 #
@@ -5,5 +6,12 @@ set -e
 aws cloudformation validate-template --template-body file://cloudformation.yml
 aws cloudformation delete-stack --stack-name monkeyStack
 aws cloudformation wait stack-delete-complete --stack-name monkeyStack
-aws cloudformation deploy --template-file ./cloudformation.yml --stack-name monkeyStack --capabilities CAPABILITY_IAM \
+
+set +e
+if aws cloudformation deploy --template-file ./cloudformation.yml --stack-name monkeyStack --capabilities CAPABILITY_IAM \
     --parameter-overrides S3BucketName=chipmonkey.json
+then
+aws cloudformation describe-stacks --stack-name monkeyStack
+else
+aws cloudformation describe-stack-events --stack-name monkeyStack
+fi
