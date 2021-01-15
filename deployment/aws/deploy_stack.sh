794feb36a6b23d9207c89206c4c6c53ff74c2fe5
Chip Lynch <chip@chiplynch.com>
2021-01-14 16:00:45 -0800

    ACTUALLY fix S3BUCKET config in deployment scripts

--- deployment/aws/deploy_stack.sh
+++ deployment/aws/deploy_stack.sh
@@ -3,12 +3,16 @@
 # Requires lambda_deployfile.zip is already pushed to the S3 deployment bucket
 # Will fail if stack already exists
 #
+
+# Get S3BUCKET from environment or use default:
+S3BUCKET=${S3BUCKET:-chipmonkey.json}
+
 set -e
 aws cloudformation validate-template --template-body file://cloudformation.yml
 
 set +e
 if aws cloudformation deploy --template-file ./cloudformation.yml --stack-name monkeyStack --capabilities CAPABILITY_IAM \
-    --parameter-overrides S3BucketName=chipmonkey.json
+    --parameter-overrides S3BucketName=$(S3BUCKET)
 then
 aws cloudformation describe-stacks --stack-name monkeyStack
 else
--- deployment/aws/update_stack.sh
+++ deployment/aws/update_stack.sh
@@ -3,13 +3,16 @@
 # Updates existing aws stack
 #
 
+# Get S3BUCKET name from environment variable or use default
+S3BUCKET=${S3BUCKET:-chipmonkey.json}
+
 set -e
-aws s3 cp ./cloudformation.yml s3://chipmonkey.json/
+aws s3 cp ./cloudformation.yml s3://$(S3BUCKET)/
 # Parameterize this:
-# aws lambda update-function-code --function-name monkeyStack-JSONLambda-AQHGX1GQFX8O --s3-bucket chipmonkey.json --s3-key lambda_deployfile.zip
+# aws lambda update-function-code --function-name monkeyStack-JSONLambda-JR87DLJY63E6  --s3-bucket $(S3BUCKET) --s3-key lambda_deployfile.zip
 aws cloudformation validate-template --template-body file://cloudformation.yml
 aws cloudformation update-stack --template-url https://s3.amazonaws.com/chipmonkey.json/cloudformation.yml \
   --stack-name monkeyStack \
   --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=S3BucketName,ParameterValue=chipmonkey.json
+  --parameters ParameterKey=S3BucketName,ParameterValue=$(S3BUCKET)
 
